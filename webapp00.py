379e94b29cc849779e49fb110829bfbec1835b18
luizhsdj <2306489@escolas.anchieta.br>
2024-10-17 20:44:03 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -11,13 +11,13 @@ Configurar_Pagina("Exemplo 1",
                     "©️")
 
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Quero minha casa")
+st.title("meu primeiro site")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header(" to cansado")
+st.header("  cabeçalho")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("quero dormir")
+st.subheader("subtitulo")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
