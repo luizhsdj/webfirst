fe28f21db2db6058e8ca7ffa6f475d73d27ba6b5
luizhsdj <2306489@escolas.anchieta.br>
2024-10-17 20:38:22 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -17,7 +17,7 @@ st.title("luiz henrique")
 st.header("mestre do jogo")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("Que jogo?")
 
 # Use st.write("") para adicionar um texto ao seu Web app
 st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
